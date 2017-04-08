81c49ab54d9b5c33c744fc9622ccd9cda18cc45c
crayclay <clemensreinhard95@gmail.com>
2017-04-08 14:20:35 +0200

    Add files via upload

---
+++ Artifakt/src/main/java/Klassen/Artifakt/Brett.java
@@ -0,0 +1,19 @@
+package Klassen.Artifakt;
+
+public class Brett {
+
+	/*
+	int sizeX;
+	int sizeY;
+	
+	// initialisieren des Bretts
+	public Brett (int x, int y) {
+		//int [][] brett = new int [x][y];
+		this.sizeX = x;
+		this.sizeY = y;
+	}
+	*/
+	
+	public static int [][] brett = new int [Game.fieldSizeX][Game.fieldSizeY];
+	
+}
---
+++ Artifakt/src/main/java/Klassen/Artifakt/Main.java
@@ -0,0 +1,10 @@
+package Klassen.Artifakt;
+
+public class Main {
+
+	public static void main (String [] args) {
+		Game.showMenu();
+	}
+	
+	
+}
