bef6e365e738e1fdc37b102c585c31eb856baf44
Max <Max@Bang-Bus>
2017-04-16 12:13:28 +0200

    Alles bisherige eingebunden in die Maven Version der HDM-MI, sodass da alle 'tools' für logging und junit testing zur Verfügung stehen

---
+++ schiffe-final-mit-hdm-maven-version/src/main/java/boatbusters/schiffeversenken/schiffe_final_mit_hdm_maven_version/Brett.java
@@ -0,0 +1,21 @@
+package boatbusters.schiffeversenken.schiffe_final_mit_hdm_maven_version;
+
+
+
+public class Brett {
+
+	/*
+	int sizeX;
+	int sizeY;
+	
+	// initialisieren des Bretts
+	public Brett (int x, int y) {
+		//int [][] brett = new int [x][y];
+		this.sizeX = x;
+		this.sizeY = y;
+	}
+	*/
+	
+	public static int [][] brett = new int [Game.fieldSizeX][Game.fieldSizeY];
+	
+}
---
+++ schiffe-final-mit-hdm-maven-version/src/main/java/boatbusters/schiffeversenken/schiffe_final_mit_hdm_maven_version/Main.java
@@ -0,0 +1,14 @@
+package boatbusters.schiffeversenken.schiffe_final_mit_hdm_maven_version;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main (String [] args) {
+		Scanner scan = new Scanner(System.in);
+		Game.showMenu();
+	}
+	
+	
+}
