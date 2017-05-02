9df1a59f79f0c205d8374b90a4bc018cacc9b644
Reinhard Clemens <cr093@hdm-stuttgart.de>
2017-05-02 14:21:52 +0200

    mooooaare factories

--- boatbusters/src/main/java/c2f/boatbusters/classes/Board.java
+++ boatbusters/src/main/java/c2f/boatbusters/classes/Board.java
@@ -2,4 +2,7 @@ package c2f.boatbusters.classes;
 
 public class Board {
 
+	public static final int fieldSizeX = 10;
+	public static final int fieldSizeY = fieldSizeX;
+	
 }
--- boatbusters/src/main/java/c2f/boatbusters/factories/BoardFactory.java
+++ boatbusters/src/main/java/c2f/boatbusters/factories/BoardFactory.java
@@ -1,5 +1,18 @@
 package c2f.boatbusters.factories;
 
+import c2f.boatbusters.classes.*;
+
+
 public class BoardFactory {
 
+	public void createBoard(int i) {
+		if (i == 1) {
+			Board board1[][] = new Board[Board.fieldSizeX][Board.fieldSizeY];
+		} else if (i == 2) {
+			Board board2[][] = new Board[Board.fieldSizeX][Board.fieldSizeY];
+		} else {
+			// TODO error log here
+		
+		}
+	}
 }
