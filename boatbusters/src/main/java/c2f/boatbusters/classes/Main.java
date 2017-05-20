852201c9c50636245f99b8c10ad95ad53b76068f
Daubenschuetz Fabian <fd032@hdm-stuttgart.de>
2017-05-20 19:54:39 +0200

    Menü überarbeitet. MenuButtonklasse implementiert und Ansätze für untermenüs

--- boatbusters/src/main/java/c2f/boatbusters/classes/Main.java
+++ boatbusters/src/main/java/c2f/boatbusters/classes/Main.java
@@ -1,93 +1,149 @@
 package c2f.boatbusters.classes;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 	
-	public static void main (String [] args) {
-		launch(args);
-		
-		File dataFile = new File("bestenliste.csv"); // Eingelesene Datei
-		try (Scanner reader = new Scanner(dataFile).useDelimiter("\n")){ 
+	private GameMenu gameMenu;
 	
-			while (reader.hasNext()) { // Einlesen der schon gespeicherten Spieler
-				String[] dataArray = new String[2]; // Erstellt Array  (Zwischenspeicher)
-				dataArray = reader.next().split(";", -1); // Teilen am ';'
-				// Erstelle Spieler und füge sie der Liste hinzu
-				Highscore.bestenliste.add(new Player(dataArray[0], dataArray[1])); 
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-		Game game = new Game(0);
-		game.showMenu(game);
-		
-		
-		
-		
-		
-	}
-
 	@Override
 	
 		public void start(Stage window) throws Exception {
-			VBox root = new VBox();
-			root.setAlignment(Pos.CENTER);
 			
-			Scene scene = new Scene(root, 300, 200);
-			
-			Button btn1 = new Button("Spiel Starten");
-			Button btn2 = new Button("Highscore");
-			Button btn3 = new Button("Beenden");
-			
-			root.getChildren().add(btn1);
-			btn1.setOnAction(new EventHandler<ActionEvent>() {
+		Pane root = new Pane();
+		root.setPrefSize(800,600);
+		
+		InputStream is = Files.newInputStream(Paths.get("src/main/resources/ShipBg.jpg"));
+		Image img = new Image(is);
+		is.close();
+		
+		ImageView imgView = new ImageView(img);
+		imgView.setFitWidth(800);
+		imgView.setFitHeight(640);
+		
+		gameMenu = new GameMenu();
+		
+		root.getChildren().addAll(imgView, gameMenu);
+		
+		Scene scene = new Scene(root);
+		
+		window.setTitle("Boatbusters");
+		window.setScene(scene);
+		window.show();	
 
-				@Override
-				public void handle(ActionEvent event) {
-					System.out.println("berechne krasse graphics..");
-					System.out.println("Ihr PC kommt auf die krassen graphics nicht klar.. zurück zu menu");
-					
-				}
+	}
+	
+	private static class MenuButton extends StackPane{
+		private Text text;
+		
+		public MenuButton(String name){
+			text = new Text(name);
+			text.setFont(text.getFont());
+			text.setFill(Color.WHITE);
+			
+			Rectangle bg = new Rectangle(250,30);		//Hintergrund Buttons
+			bg.setOpacity(0.6);							//sichtbarkeit
+			bg.setFill(Color.BLACK);
+			
+			
+			setAlignment(Pos.CENTER);
+			getChildren().addAll(bg, text); // text steht über hintergrund
+			
+			//wenn maus über menupoint
+			setOnMouseEntered(event -> {
+				bg.setTranslateX(10);
+				text.setTranslateX(10);
+				bg.setFill(Color.WHITE);
+				text.setFill(Color.BLACK);
+			
+			});
+			//wenn maus menupoint verlässt
+			setOnMouseExited(event -> {
+				bg.setTranslateX(0);
+				text.setTranslateX(0);
+				bg.setFill(Color.BLACK);
+				text.setFill(Color.WHITE);
 			});
 			
+		}
+	}
+	
+	private class GameMenu extends Parent{
+		public GameMenu(){
+			VBox menu0 = new VBox(10);
+			VBox menu1 = new VBox(10); //sub menu
 			
-			root.getChildren().add(btn2);
-			btn2.setOnAction(event -> btn2.setText("du bist letzter"));
+			menu0.setTranslateX(100);
+			menu0.setTranslateY(200);
 			
+			menu1.setTranslateX(100);
+			menu1.setTranslateY(200);
 			
-			root.getChildren().add(btn3);
-			btn3.setOnAction(
-					event -> btn3.setText("hahaha vergiss es"));
+			MenuButton btnStart = new MenuButton("START GAME");
+			btnStart.setOnMouseClicked(event ->{
+				System.out.println("Start Game..");
+			});
+			MenuButton btnScore = new MenuButton("HIGHSCORE");
+			btnScore.setOnMouseClicked(event ->{
+				System.out.println("Open Highscore");
+			});
 			
-			window.setTitle("Boatbusters");
-			window.setScene(scene);
-			window.show();
-		
+			MenuButton btnExit = new MenuButton("Exit");
+			btnExit.setOnMouseClicked(event ->{
+				System.exit(0);
+			});
+			
+			menu0.getChildren().addAll(btnStart, btnScore, btnExit);
+			
+			Rectangle bg = new Rectangle(800,600);
+			bg.setFill(Color.GREY);
+			bg.setOpacity(0.4);
+			
+			getChildren().addAll(bg, menu0);
+		}
 	}
 	
 	
+	
+public static void main (String [] args) {
+	launch(args);
+	
+	File dataFile = new File("bestenliste.csv"); // Eingelesene Datei
+	try (Scanner reader = new Scanner(dataFile).useDelimiter("\n")){ 
+
+		while (reader.hasNext()) { // Einlesen der schon gespeicherten Spieler
+			String[] dataArray = new String[2]; // Erstellt Array  (Zwischenspeicher)
+			dataArray = reader.next().split(";", -1); // Teilen am ';'
+			// Erstelle Spieler und füge sie der Liste hinzu
+			Highscore.bestenliste.add(new Player(dataArray[0], dataArray[1])); 
+		}
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	}
+	
+	
+	Game game = new Game(0);
+	game.showMenu(game);
+	
+}
 }
