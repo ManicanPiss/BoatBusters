5c5c5d9cb54b10d9ca8352ecb3ddb3d868d49840
Max <Max@Bang-Bus>
2017-05-23 06:53:51 +0200

    ArraySortier Methode verbessert
    die hat gar nichts mehr gemacht solange derjenige mit den meisten Wins
    ganz oben stand, d.h. fritz mit 100 Wins

--- boatbusters/src/main/java/c2f/boatbusters/classes/Highscore.java
+++ boatbusters/src/main/java/c2f/boatbusters/classes/Highscore.java
@@ -17,39 +17,41 @@ public class Highscore {
 		Main.getLogger().info("Player            | NumberOfWins\n------------------+---------------");
 		for (int i = 0; i < bestenliste.size(); i++){
 			Main.getLogger().info(bestenliste.get(i));
-	
+			
 		}
 	}
 	
-    public static int searchForPlayerWithMostWins(){
-	   int mostWins = Integer.parseInt((String) bestenliste.get(0).numberOfWins());
-	   int indexOfPlayerWithMostWins = 0;
-    for (int i = 1; i < bestenliste.size(); i++){
- 	   if  (Integer.parseInt((String) bestenliste.get(i).numberOfWins()) >  mostWins){
- 		   mostWins = Integer.parseInt((String) bestenliste.get(i).numberOfWins());
- 		   indexOfPlayerWithMostWins = i;
- 	   } 
-    }
-    return indexOfPlayerWithMostWins;
-}
-    
-    
-    
-    
-    
+   // public static int searchForPlayerWithMostWins(){
+   //	   int mostWins = Integer.parseInt((String) bestenliste.get(0).numberOfWins());
+   //	   int indexOfPlayerWithMostWins = 0;
+   // for (int i = 1; i < bestenliste.size(); i++){
+   //   if  (Integer.parseInt((String) bestenliste.get(i).numberOfWins()) >  mostWins){
+   //	   mostWins = Integer.parseInt((String) bestenliste.get(i).numberOfWins());
+   //	   indexOfPlayerWithMostWins = i;
+   // } 
+   //}
+   //return indexOfPlayerWithMostWins;
+   //}
+
     public static void sortArrayList(){
-    int indexOfPlayerWithMostWins = searchForPlayerWithMostWins();
-    for (int j = 0; j < indexOfPlayerWithMostWins; j++){
-    for (int i = 0; i < bestenliste.size()-1; i++){
     
-    if((Integer.parseInt((String) bestenliste.get(i).numberOfWins()) - (Integer.parseInt((String) bestenliste.get(i+1).numberOfWins())) < 0)){
-       Player o = bestenliste.get(i);
- 	   bestenliste.set(i, bestenliste.get(i+1));
- 	   bestenliste.set(i+1, o);
-      }
-    }
+    	boolean sortOn = true;
+    	while (sortOn){
+    	  boolean wrongOrder = false;
+    	  for (int i = 0; i < bestenliste.size()-1; i++){
+    	        
+    	  if((Integer.parseInt((String) bestenliste.get(i).numberOfWins())
+    			  - (Integer.parseInt((String) bestenliste.get(i+1).numberOfWins())) < 0)){
+    	           Player o = bestenliste.get(i);
+    	     	   bestenliste.set(i, bestenliste.get(i+1));
+    	     	   bestenliste.set(i+1, o);
+    	     	   wrongOrder = true;
+              }
+          }
+    	  sortOn = wrongOrder;
+       }
   } 
-}
+
     
     public static String checkIfArrayListContainsName(String namePlayer1){
     	for (int i = 0; i < bestenliste.size(); i++){
